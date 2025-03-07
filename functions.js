c68e59ee1f7d79231318475fc374f5be2414380e
benjastro <benji3.astro@gmail.com>
2025-03-07 11:11:31 -0800

    Add mike debt and xfer presets

---
+++ functions.js
@@ -0,0 +1,22 @@
+function createTransferPresetsPanel() {
+    panel = document.createElement("div");
+    panel.style.border = "2px solid black";
+    panel.style.boxSizing = "border-box";
+    panel.style.padding = "4px";
+    panel.style.minWidth = "300px";
+    
+    panel.innerHTML = 
+    `<h2>Transfer Presets</h2>
+    <p>
+        <strong>DEBT OB</strong> - All outbound converted debt transfer
+        <br><br><strong>DEBT AFF</strong> - All affs converted debt transfer
+        <br><br><strong>DEBT ADSYNC</strong> - All adsync converted debt transfer
+    </p>
+    `;
+
+    return panel;
+}
+
+function putElement(id, element) {
+    document.getElementById(id).appendChild(element);
+}
